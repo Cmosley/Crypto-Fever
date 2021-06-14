eaccce76578baec307d84b31ecb8ccdf71d8da83
cmosley <cmosley@beholdmedia.com>
2021-06-14 08:36:08 -0500

    refactor hooks to app page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Route, Redirect, useHistory } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 import Signup from "../Signup/Signup";
@@ -7,6 +7,106 @@ import authService from "../../services/authService"
 import "./App.css";
 
 function App (props) {
+  const [currencies, setcurrencies] = useState([]);
+  const [pair, setpair] = useState("");
+  const [price, setprice] = useState("0.00");
+  const [pastData, setpastData] = useState({});
+  const ws = useRef(null);
+
+  let first = useRef(false);
+  const url = "https://api.pro.coinbase.com";
+
+  // websocket connection to coinbase api to return price values 
+  useEffect(() => {
+    ws.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
+
+    let pairs = [];
+
+    const apiCall = async () => {
+      await fetch(url + "/products")
+        .then((res) => res.json())
+        .then((data) => (pairs = data));
+      
+      let filtered = pairs.filter((pair) => {
+        if (pair.quote_currency === "USD") {
+          return pair;
+        }
+      });
+
+      filtered = filtered.sort((a, b) => {
+        if (a.base_currency < b.base_currency) {
+          return -1;
+        }
+        if (a.base_currency > b.base_currency) {
+          return 1;
+        }
+        return 0;
+      });
+
+      
+      setcurrencies(filtered);
+
+      first.current = true;
+    };
+
+    apiCall();
+  }, []);
+
+  useEffect(() => {
+    if (!first.current) {
+      
+      return;
+    }
+
+    
+    let msg = {
+      type: "subscribe",
+      product_ids: [pair],
+      channels: ["ticker"]
+    };
+    let jsonMsg = JSON.stringify(msg);
+    ws.current.send(jsonMsg);
+
+    let historicalDataURL = `${url}/products/${pair}/candles?granularity=86400`;
+    const fetchHistoricalData = async () => {
+      let dataArr = [];
+      await fetch(historicalDataURL)
+        .then((res) => res.json())
+        .then((data) => (dataArr = data));
+      
+      let formattedData = formatData(dataArr);
+      setpastData(formattedData);
+    };
+
+    fetchHistoricalData();
+
+    ws.current.onmessage = (e) => {
+      let data = JSON.parse(e.data);
+      if (data.type !== "ticker") {
+        return;
+      }
+
+      if (data.product_id === pair) {
+        setprice(data.price);
+      }
+    };
+  }, [pair]);
+
+  const handleSelect = (e) => {
+    let unsubMsg = {
+      type: "unsubscribe",
+      product_ids: [pair],
+      channels: ["ticker"]
+    };
+    let unsub = JSON.stringify(unsubMsg);
+
+    ws.current.send(unsub);
+
+    setpair(e.target.value);
+  };
+
+
+
   const [user, setUser] = useState(authService.getUser())
   const history = useHistory();
 
